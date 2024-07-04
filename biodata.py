1accadb4b9500bc60185cb7ff3f3c09ed5dc7118
RiyoPhnca <161007588+RiyoPhnca@users.noreply.github.com>
2024-07-04 20:12:09 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,18 @@
+# Tugas Dasar Pemrograman
+# Biodata Sederhana
+
+nama = "Riyo Phanca"
+kelas = "TI23M"
+jurusan = "Teknik Informatika"
+quote = "Always deliver more than expected"
+
+biodata = f"""
+Biodata Sederhana:
+
+Nama    : {nama}
+Kelas   : {kelas}
+Jurusan : {jurusan}
+Quote   : {quote}
+"""
+
+print(biodata)
